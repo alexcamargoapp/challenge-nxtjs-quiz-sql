873b421a8a3bb2d52de709d6dd21a8aa51d8b093
alexcamargoapp <alexcamargo.app@gmail.com>
2021-01-27 20:21:54 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -35,13 +35,13 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>AluraQuiz - Modelo Base</title>
+        <title>Alex Camargo - Quiz SQL</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>The legend of zelda</h1>
+            <h1>SQL ANSY COMMANDS</h1>
           </Widget.Header>
           <Widget.Content>
             <form onSubmit={function (infosDoEvento) {
@@ -57,7 +57,7 @@ export default function Home() {
                   // name = infosDoEvento.target.value;
                   setName(infosDoEvento.target.value);
                 }}
-                placeholder="Diz ai seu nome"
+                placeholder="Seu nome"
               />
               <button type="submit" disabled={name.length === 0}>
                 Jogar
@@ -76,7 +76,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/alexcamargoapp/challenge-nxtjs-quiz-sql" />
     </QuizBackground>
   );
 }
